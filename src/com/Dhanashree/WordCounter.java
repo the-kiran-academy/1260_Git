a1db03c44a8740148c8d923749ccd63db207f70a
unknown <dhanashreesonawane378@gmail.com>
2025-07-02 00:26:48 -0500

    check WordCounter

--- src/com/Dhanashree/WordCounter.java
+++ src/com/Dhanashree/WordCounter.java
@@ -5,18 +5,15 @@ public class WordCounter {
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 
-	        // Input the string
 	        System.out.print("Enter a string: ");
 	        String input = scanner.nextLine();
 
 	        int wordCount = 0;
 	        boolean inWord = false;
 
-	        // Loop through each character
 	        for (int i = 0; i < input.length(); i++) {
 	            char ch = input.charAt(i);
-
-	            // Check if the character is a space or not
+	            
 	            if (ch != ' ' && ch != '\t' && ch != '\n') {
 	                if (!inWord) {
 	                    wordCount++;
