0e379ce76022fe79b43ec216cf345d20c2bddf39
Saurabhkakad01 <saurabhkakad57@gmail.com>
2025-07-02 11:46:21 +0530

    Even_Odd

---
+++ src/com/saurabh/Even_Odd.java
@@ -0,0 +1,13 @@
+package com.saurabh;
+
+public class Even_Odd {
+public static void main(String[] args) {
+	int a = 10;
+	if(a%2==0) {
+		System.out.println("Even Number..");
+	}
+	else {
+		System.out.println("Odd Number..");
+	}
+}
+}
