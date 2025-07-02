a11ecc27f659a9c4ae6f8b33d2eecb724b2bdc51
RohitSikchi <rohitsikchi866@gmail.com>
2025-07-02 09:41:40 +0530

    Largest Three Number

---
+++ src/com/rohit_sikchi/LargeThreeNum.java
@@ -0,0 +1,32 @@
+package com.rohit_sikchi;
+
+import java.util.Scanner;
+
+public class LargeThreeNum {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Three Number : ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int largest;
+
+		if (a >= b) {
+			if (a >= c) {
+				largest = a;
+			} else {
+				largest = c;
+			}
+		} else {
+			if (b >= c) {
+				largest = b;
+			} else {
+				largest = c;
+			}
+		}
+
+		System.out.println("Largest Num : " + largest);
+
+	}
+}
