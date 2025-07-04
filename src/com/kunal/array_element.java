cec665b7e4b1e88dd15468aacb13d7957ada69ee
kunal <dhoknekunal9@gmail.com>
2025-07-04 13:18:50 +0530

    array_of_element

---
+++ src/com/kunal/array_element.java
@@ -0,0 +1,22 @@
+package com.kunal;
+
+public class array_element {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int[] arr = {5, 8, 2, 10, 3};
+    int largest = arr[0];
+    
+    for(int i = 1; i < arr.length; i++)
+    {
+    	if(arr[i] > largest)
+    	{
+    		largest = arr[i];
+    	}
+    }
+    
+    	System.out.println("The largest element is: " + largest);
+   
+	}
+
+}
